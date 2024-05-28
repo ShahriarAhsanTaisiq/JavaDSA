af87682f39a17cd475ec0e6a0cfd5e70dba51898
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-05-28 13:28:09 +0600

    Minimum cost to fill given weight in a bag  (28-05-24)

--- src/GFGRegularProblem/GFG_28May.java
+++ src/GFGRegularProblem/GFG_28May.java
@@ -4,9 +4,7 @@ package GFGRegularProblem;
 Note:
 1. cost[i] = -1 means that i kg packet of orange is unavailable.
 2. It may be assumed that there is an infinite supply of all available packet types.
-
 Example 1:
-
 Input:
 n = 5
 cost[] = {20, 10, 4, 50, 100}
@@ -16,7 +14,6 @@ Output:
 Explanation:
 Purchase the 2kg packet for 10 coins and the 3kg packet for 4 coins to buy 5kg of oranges for 14 coins.
 Example 2:
-
 Input:
 n = 5
 cost[] = {-1, -1, 4, 3, -1}
@@ -27,10 +24,8 @@ Explanation:
 It is not possible to buy 5 kgs of oranges.
 Your Task:
 You don't need to read input or print anything. Complete the function minimumCost() which takes integers n and w, and integer array cost[] as input parameters and returns the minimum cost to buy exactly w kg of oranges, If buying exactly w kg of oranges is impossible, then return -1.
-
 Expected Time Complexity: O(n*w)
 Expected Auxiliary Space: O(n*w)
-
 Constraints:
 1 ≤ n, w ≤ 2*102
 1 ≤ cost[i] ≤ 105
