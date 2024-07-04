298e77faf4bfcfff2094cbd26b53eec4568bcadd
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-07-04 19:09:33 +0600

    BinaryTree Implementation with preorder, inorder, postorder and level order traversal (04-07-24)

---
+++ src/DS/BinaryTreesImpl.java
@@ -0,0 +1,100 @@
+package DS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinaryTree{
+    int index = -1;
+    public Node buildTree(int [] nodes){
+        index++;
+        if (nodes[index] == -1){
+            return null;
+        }
+        Node newNode = new Node(nodes[index]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    public void preOrder(Node root){
+        if (root == null){
+            return;
+        }
+        System.out.print(root.val+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public void inOrder(Node root){
+        if (root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val+ " ");
+        inOrder(root.right);
+
+    }
+    public void postOrder(Node root){
+        if (root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val+" ");
+    }
+
+    public void levelOrder(Node root){
+        if (root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (! q.isEmpty()){
+            Node currNode = q.remove();
+            if (currNode==null){
+                System.out.println();
+                if (q.isEmpty()){
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.val+" ");
+                if (currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+}
+public class BinaryTreesImpl {
+    public static void main(String [] args){
+        int [] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.val);
+        System.out.println("\nPre Order Traversal");
+        tree.preOrder(root);
+        System.out.println("\nInorder Traversal");
+        tree.inOrder(root);
+        System.out.println("\nPost Order Traversal");
+        tree.postOrder(root);
+        System.out.println("\nLevel Order Traversal");
+        tree.levelOrder(root);
+
+    }
+}
